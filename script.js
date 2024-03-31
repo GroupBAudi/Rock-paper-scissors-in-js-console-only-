bacd633aba23b8e8677fe49aef72af07ee9dbd84
Audi <144666972+GroupBAudi@users.noreply.github.com.>
2024-04-01 06:17:42 +0700

    Adds winning system (idk the name)

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-document.write("Connected in chinese voice 🔵🔴 - Check the console I mean.");
+/* document.write("Connected in chinese voice 🔵🔴 - Check the console I mean."); */
 let getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3) + 1; /* gets random number from number 1 to 3 */
-    console.log(choice);
+    switch (choice) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors"
+            break;
+    }
 }
 
-let playerSelection = () => {
-    let playerChoice = prompt("Select your Choice 😏 (Only type 'Rock', 'Paper', or 'Scissors' [case insensitive] though, as regex hasn't been implemented yet)");
-    console.log(playerChoice.toLowerCase());
+let getPlayerChoice = () => {
+   let playerSelection = prompt("Select your Choice 😏 (Only type 'Rock', 'Paper', or 'Scissors' [case insensitive] though, as regex hasn't been implemented yet)");
+   /* let playerSelection = "sCisSoRs" */ 
+   let playerChoice = playerSelection.toLowerCase();
+   let arr = playerChoice.split("");
+   let firstWord = arr[0].toUpperCase();
+   arr.splice(0, 1, firstWord); 
+   return arr.join("");  /* returns player choice */
 }
 
-let computerSelection = () => {
-    return getComputerChoice(); /* at least it works */
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You lose! Rock beats Scissors"
+    } else  if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You win! Rock beats Scissors"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You lose! Scissors beats Paper"
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You win! Scissors beats Paper"
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You lose! Paper beats Rock"
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You win! Paper beats Rock"
+    } else if (playerSelection == computerSelection) {
+        return "Tie"
+    } else {
+        return "What did you even choose? (error)"
+    }
 }
 
-console.log(computerSelection());
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
